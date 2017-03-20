a557af79321fef405ed76a96d7f56b51c0c1aece
LamSee <l1727629@student.southstaffs.ac.uk>
2017-03-20 11:53:17 +0000

    Initial Project Upload
    My complete project for Web Development

---
+++ js/buttons.js
@@ -0,0 +1,80 @@
+function functionUncharted() {
+    var x = document.getElementById('uncharted');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionTitanfall() {
+    var x = document.getElementById('titanfall');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionResident() {
+    var x = document.getElementById('resident');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionOverwatch() {
+    var x = document.getElementById('overwatch');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionHonor() {
+    var x = document.getElementById('forhonor');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionFinal() {
+    var x = document.getElementById('ffxv');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionDishonored() {
+    var x = document.getElementById('dishonored');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionSouls() {
+    var x = document.getElementById('souls');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function functionBattlefield() {
+    var x = document.getElementById('battlefield');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
